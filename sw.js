2447c6722564234e881ac5ceb4ebd60b6cd04d4b
Melissa Amenuveve <108826517+Mamenuveve2201@users.noreply.github.com>
2026-03-13 03:06:36 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE = 'networth-v3';
+var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).then(function(res) {
+      var clone = res.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, clone); });
+      return res;
+    }).catch(function() {
+      return caches.match(e.request).then(function(r){ return r || caches.match('./index.html'); });
+    })
+  );
+});
